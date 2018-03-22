ec727dd0585c6af0e61d3c94f82901e92589a953
Sebastian Netsch <sebastian.netsch@hotmail.com>
2018-03-22 19:26:06 +0100

    Restructured project
    outsourced several functions into own classes
    changed README.md to represent an actual "CSharp Updater.exe" call

---
+++ Source/CSharp Updater/Logger.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CSharp_Updater
+{
+    class Logger
+    {
+        public static void Log(string pathToLogFile, string message, [CallerMemberName]string callerMember = "")
+        {
+            try
+            {
+                using (StreamWriter file = new StreamWriter(pathToLogFile, true))
+                {
+                    /* get caller class and method name */
+                    StackFrame frame = GetLastStackFrame();
+
+                    file.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:MM:ss: ") + frame.GetMethod().DeclaringType.ToString() + frame.GetMethod().Name + ": " + message);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+
+                /* can't log the log error.. */
+            }
+        }
+
+        public static void Log(string pathToLogFile, Exception ex, [CallerMemberName]string callerMember = "")
+        {
+            try
+            {
+                using (StreamWriter file = new StreamWriter(pathToLogFile, true))
+                {
+                    /* get caller class and method name */
+                    StackFrame frame = GetLastStackFrame();
+
+                    file.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:MM:ss: ") + frame.GetMethod().DeclaringType.ToString() + frame.GetMethod().Name + ": " + ex.Message);
+                }
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show(e.Message);
+
+                /* can't log the log error.. */
+            }
+        }
+
+        public static StackFrame GetLastStackFrame()
+        {
+            return new StackFrame(2); // get on top of this and caller function
+        }
+    }
+}
---
+++ Source/CSharp Updater/Utility.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharp_Updater
+{
+    class Utility
+    {
+        public static bool DoChangeLabel(System.Windows.Forms.Label label, string content, string logPath)
+        {
+            try
+            {
+                label.Text = content;
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(logPath, ex);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        public static bool DoChangeProgress(System.Windows.Forms.ProgressBar progress, int percentage, string logPath)
+        {
+            try
+            {
+                progress.Value = percentage;
+            }
+            catch (Exception ex)
+            {
+                Logger.Log(logPath, ex);
+
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
